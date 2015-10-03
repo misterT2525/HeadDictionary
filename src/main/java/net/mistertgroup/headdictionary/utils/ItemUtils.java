f79312a77f5147b61b3df8b60c5c7b0f3db2709e
misterT2525 <info@mistertgroup.net>
2015-10-03 01:30:26 -0700

    Add set display name and lore method to ItemUtils

--- src/main/java/net/mistertgroup/headdictionary/utils/ItemUtils.java
+++ src/main/java/net/mistertgroup/headdictionary/utils/ItemUtils.java
@@ -4,6 +4,8 @@ import lombok.NonNull;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
+
 /**
  * @author misterT2525
  */
@@ -16,6 +18,17 @@ public final class ItemUtils {
     public static void setDisplayName(@NonNull ItemStack item, String name) {
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(name);
+        item.setItemMeta(meta);
+    }
+
+    public static void setDisplayNameAndLore(@NonNull ItemStack item, String name, String... lore) {
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        if (lore != null && lore.length > 0) {
+            meta.setLore(Arrays.asList(lore));
+        } else {
+            meta.setLore(null);
+        }
         item.setItemMeta(meta);
     }
 }
